1c5ffdf9247ab80be60f1e90492fdc1ae9aabb20
InsertNicknameHere00 <rtonev11@gmail.com>
2024-08-06 17:50:09 +0300

    Added DBset for Users

--- Data/HaircutSalonDbContext.cs
+++ Data/HaircutSalonDbContext.cs
@@ -7,7 +7,7 @@ namespace AppointmentAPI.Data
 {
     public class HaircutSalonDbContext : DbContext
     {
-
+        public DbSet<Users> Users { get; set; }
         public DbSet<SalonService> SalonService { get; set; }
         public DbSet<AdminService> AdminServices { get; set; }
         public HaircutSalonDbContext(DbContextOptions<HaircutSalonDbContext> options) : base(options)
