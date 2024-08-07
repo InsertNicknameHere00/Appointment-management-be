3fa9b215f220643788a47acd14e500d95061eba3
marko-panaiotov <marko.panaiotov@gmail.com>
2024-08-07 15:14:02 +0300

    fixed

--- Data/MSSQL Database Scripts/Create_Appointsments_Table.sql
+++ Data/MSSQL Database Scripts/Create_Appointsments_Table.sql
@@ -1,10 +1,10 @@
 USE haircutSalonDB;
 GO
 
-CREATE TABLE Appointsments(
-appointsmentID int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-appointsmentStatus int NOT NULL,
-appointsmentStartDate DateTime2,
-appointsmentEndDate DateTime2,
+CREATE TABLE Appointments(
+appointmentID int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+appointmentStatus int NOT NULL,
+appointmentStartDate DateTime2,
+appointmentEndDate DateTime2,
 userID int FOREIGN KEY REFERENCES Users(UserID) not null
 );	
