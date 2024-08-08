9524c42a3090382fce589db62df7127aa0696922
marko-panaiotov <marko.panaiotov@gmail.com>
2024-08-08 17:02:20 +0300

    minor changes
    some fixes

--- Services/EmailSendService.cs
+++ Services/EmailSendService.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Options;
 using MimeKit;
 
 
-namespace AppointmentAPI.Repository
+namespace AppointmentAPI.Services
 {
     public class EmailSendService : IEmailSendService
     {
