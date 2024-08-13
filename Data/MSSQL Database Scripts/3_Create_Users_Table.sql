29c0aaac37665c361ba52d3a6dc7a95c411adc94
marko-panaiotov <marko.panaiotov@gmail.com>
2024-08-13 13:26:35 +0300

    Update 3_Create_Users_Table.sql

--- Data/MSSQL Database Scripts/3_Create_Users_Table.sql
+++ Data/MSSQL Database Scripts/3_Create_Users_Table.sql
@@ -5,7 +5,7 @@ CREATE TABLE Users(
 	UserID int IDENTITY(1,1) PRIMARY KEY NOT NULL,
 	FirstName varchar(100) NOT NULL,
 	LastName varchar(100) NOT NULL,
-	Email varchar(50) NOT NULL,
+	Email varchar(50) NOT NULL UNIQUE,
 	PhoneNumber varchar(10) NOT NULL,
 	PasswordHash varchar(255) NOT NULL,
 	RoleID int NOT NULL FOREIGN KEY REFERENCES Roles(RoleID),
