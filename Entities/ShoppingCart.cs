a53034d416506e6d1f57340419c926853808e855
Erika Karamucheva <vesselinova11@gmail.com>
2024-08-13 13:21:06 +0300

    Add Product

---
+++ Entities/ShoppingCart.cs
@@ -0,0 +1,7 @@
+﻿namespace AppointmentAPI.Entities
+{
+    public class ShoppingCart
+    {
+        
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using AppointmentAPI.Entities;
+
+namespace AppointmentAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> Save(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> Update(int id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
