33d7bebddddf7f764b87b8685d2a5a6822d937a7
Erika Karamucheva <vesselinova11@gmail.com>
2024-08-14 14:38:24 +0300

    Added OrderItem

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppointmentAPI.Entities
+{
+    public class OrderItem
+    {
+        [ForeignKey("OrderId")]
+        public int OrderId { get; set; }
+        [ForeignKey("ProductId")]
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
