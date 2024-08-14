ca66c493b1703d34100e172fe761109e23352584
Erika Karamucheva <vesselinova11@gmail.com>
2024-08-14 14:57:15 +0300

    fixed

---
+++ Repository/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using AppointmentAPI.Entities;
+
+namespace AppointmentAPI.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<Order> AddOrder(Order order);
+        Task<Order> UpdateOrder(Order order);
+
+    }
+}
---
+++ Repository/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using AppointmentAPI.Data;
+using AppointmentAPI.Entities;
+
+namespace AppointmentAPI.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly HaircutSalonDbContext _context;
+        public OrderRepository(HaircutSalonDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<Order> AddOrder(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+            return order;
+        }
+
+        public Task<Order> UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
