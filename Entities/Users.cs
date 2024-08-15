f382491271d2f89b28f246fa4eeff31f8a64c4c5
marko-panaiotov <marko.panaiotov@gmail.com>
2024-08-15 13:31:16 +0300

    Added column verifiedStatus in database
    Added column verifiedStatus in database

--- Entities/Users.cs
+++ Entities/Users.cs
@@ -28,6 +28,9 @@
         [Required]
         public int? RoleID { get; set; }
 
+        [Required]
+        public string? VeryfiedStatus { get; set; }
+
         [ForeignKey("RoleID")]
         public virtual Role Role { get; set; }
 
